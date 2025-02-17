5bb566c2acb02febe31752a1de2a25c260d70c84
poojasree0000 <jms10b21poojasrees@gmail.com>
2025-02-17 10:04:41 -0800

    Assesment4

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
 
-
 import React from 'react'
 import './App.css'
 import Comp1 from './Comp1'
 
+function App() {
+  const myInfo={
+    name1:"Poojasree:)",
+    age1: 18,
+    roll1: "sec23cj032",
+    dept1: "M.Tech CSE"
+  }
+  const friendInfo={
+    name2:"Keerthana:)",
+    age2:18,
+    roll2: "sec23cj001",
+    dept2: "M.Tech CSE"
+  }
+  
 
-const App = () => {
-  return (
+return (
+
+    <>
+    <h1 style={{textAlign:"center",color:"purple"}}>Besties Forever</h1>
+    <br />
+
     <div>
-      <p className ='text'>App</p>
-      <Comp1 name="Sai" year="3" />
+    <h1 style={{textAlign:"center",color:"purple",fontStyle:"Romans"}}>Me</h1>
+    <Comp1  name1={myInfo.name1} age1={myInfo.age1} roll1={myInfo.roll1} dept1={myInfo.dept1} />
+    <h1 style={{textAlign:"center",color:"purple",fontStyle:"Romans"}}>She</h1>
+    <Comp1  name2={friendInfo.name2} age2={friendInfo.age2} roll2={friendInfo.roll2} dept2={friendInfo.dept2} />
     </div>
+
+
+    <button onClick={()=>alert("click is clicked")}> One click</button>
+    <button onDoubleClick={()=>alert("click is Double clicked")}>Double click</button>
+    </>
   )
 }
-
 export default App
+
--- src/Comp1.jsx
+++ src/Comp1.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+
 const Comp1 = (props) => {
-    return (
-        <div>{props.name}</div>
-    )
+  return (
+    <div>
+      <h3>{props.name1}<br></br>{props.age1}<br></br>{props.roll1}<br></br>{props.dept1}
+    {props.name2}<br></br>{props.age2}<br></br>{props.roll2}<br></br>{props.dept2}</h3>
+    
+    </div>
+    
+  )
 }
+
 export default Comp1
